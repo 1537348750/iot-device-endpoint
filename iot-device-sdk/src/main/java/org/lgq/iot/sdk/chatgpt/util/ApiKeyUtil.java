29097a6bd9cd6fb69d325dfcbdd2125c2e29c96d
LGQ <1537348750@qq.com>
2023-04-02 23:23:12 +0800

    补充chatGPT APIKey说明

--- iot-device-sdk/src/main/java/org/lgq/iot/sdk/chatgpt/util/ApiKeyUtil.java
+++ iot-device-sdk/src/main/java/org/lgq/iot/sdk/chatgpt/util/ApiKeyUtil.java
@@ -9,13 +9,19 @@ public class ApiKeyUtil {
 
     private static String APP_KEY;
 
+    /**
+     * 你需要有一个chatGPT账户，并且找到你的APIKey，在类路径创建API_KEY.json文件，
+     * 内容为：{"api_key":"sk-***********..."}
+     * 寻找api_key的地址：https://platform.openai.com/account/api-keys
+     * @return
+     */
     public static String getApiKey() {
       if (APP_KEY != null) {
           return APP_KEY;
       }
         InputStream inputStream = ApiKeyUtil.class.getClassLoader().getResourceAsStream("API_KEY.json");
         if (inputStream == null) {
-            throw new NullPointerException();
+            throw new NullPointerException("file in classpath may be null !");
         }
         Scanner scanner = new Scanner(inputStream);
         StringBuilder sb = new StringBuilder();
